6d105d9561dcb716a828e9f0cfce8270886d866f
samiam120 <samassefa3@gmail.com>
2024-09-15 10:06:52 -0700

    added basic js functionality to add books

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
 const myLibrary = ["Harry Potter", "Percy Jackson"];
-myLibrary.forEach(book =>{
-    console.log(book);
+
+const author = document.getElementById("author");
+const title = document.getElementById("title");
+const numPages = document.getElementById("numPages");
+const haveRead = document.getElementById("have-read");
+const addBtn = document.querySelector(".add");
+
+function Book(author, title, numPages, haveRead) {
+  this.author = author;
+  this.title = title;
+  this.numPages = numPages;
+  this.haveRead = haveRead;
+}
+
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+}
+
+addBtn.addEventListener("click", () => {
+  const newBook = new Book(
+    author.value,
+    title.value,
+    numPages.value,
+    haveRead.checked
+  );
+  addBookToLibrary(newBook);
+  author.value = "";
+  title.value = "";
+  numPages.value = "";
+  haveRead.checked = false;
+  console.log(myLibrary);
 });
-
-function Book(){
-
-}
-
-function addBookToLibrary(){
-    
-}
